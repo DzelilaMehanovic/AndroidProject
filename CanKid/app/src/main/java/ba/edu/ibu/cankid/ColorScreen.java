b539ac896daf93bd49bd272c4a63ef67de03206d
DzelilaMehanovic <dzelila.mehanovic@stu.ibu.edu.ba>
2016-11-05 21:32:42 +0100

    message

---
+++ CanKid/app/src/main/java/ba/edu/ibu/cankid/ColorScreen.java
@@ -0,0 +1,20 @@
+package ba.edu.ibu.cankid;
+
+/**
+ * Created by muki on 05.11.2016..
+ */
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ColorScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.colors);
+    }
+}
--- CanKid/app/src/main/java/ba/edu/ibu/cankid/MainActivity.java
+++ CanKid/app/src/main/java/ba/edu/ibu/cankid/MainActivity.java
@@ -1,7 +1,11 @@
 package ba.edu.ibu.cankid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button colorButton=(Button) findViewById(R.id.button_colors);
+
+        colorButton.setOnClickListener(new OnClickListener(){
+            public void onClick(View v) {
+                Intent colorIntent = new Intent(MainActivity.this, ColorScreen.class);
+                startActivity(colorIntent);//MainActivity.this  getApplicationContext()
+            }
+
+        });
     }
 }
